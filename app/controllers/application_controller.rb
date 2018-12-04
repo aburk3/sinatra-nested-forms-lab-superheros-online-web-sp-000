ed2b5086ec7ae8bd2c20f2c4da69f70368261deb
Austin  Burke <aburk3@gmail.com>
2018-12-04 22:48:58 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,18 +10,18 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
-      @team_name = params[:team][:name]
-      @team_motto = params[:team][:motto]
-
-      @hero_name = []
-      @hero_power = []
-      @hero_bio = []
-      @hero_members = params[:team][:members]
-      @hero_members.each do |member|
-        @hero_name << member[:name]
-        @hero_power << member[:power]
-        @hero_bio << member[:bio]
-      end
+      # @team_name = params[:team][:name]
+      # @team_motto = params[:team][:motto]
+      #
+      # # @hero_name = []
+      # # @hero_power = []
+      # # @hero_bio = []
+      # # @hero_members = params[:team][:members]
+      # # @hero_members.each do |member|
+      # #   @hero_name << member[:name]
+      # #   @hero_power << member[:power]
+      # #   @hero_bio << member[:bio]
+      # # end
       erb :team
     end
 
